f8586a9387bf227536f0cbf9f3bbaa2d6a5efbda
Paulo Wells <48473779+wellspr@users.noreply.github.com>
2019-09-18 13:54:29 -0300

    Update home.php

--- content/home.php
+++ content/home.php
@@ -1,14 +1,1 @@
-<?php
-if(isset($_SESSION['id'])){
-  echo 'Logado como ' . $_SESSION['email'] . "<br>";
-  echo 'Id de usuário: ' . $_SESSION['id'];
-  echo '<br>';
-  echo '<a href="/auth/google-login">Trocar Usuário</a>';
-  echo ' | ';
-  echo '<a href="auth/logout">Log out</a>';
-}
-else{
-  echo '<p>';
-  echo '<a href="/auth/google-login">Google Login</a>';
-  echo '</p>';
-}
+<h1>Hello World!!!</h1>
